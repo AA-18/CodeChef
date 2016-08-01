635593ba11dc0c15e143cf568e78a4ca18e2437c
Francisc Munteanu <munteanu.francisc@gmail.com>
2016-08-01 22:23:24 +0200

    Lumpybus Greedy Solution

--- july_lunchtime/Lumbybus.cpp
+++ july_lunchtime/Lumbybus.cpp
@@ -1,40 +1,65 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
+long long int N,P,Q, maxChilds;
+
+void solveByGreedy(vector<int> num){
+
+    for(int change : num){
+        long long removedFromTwos = 0; 
+        if(change <= Q * 2){
+            Q -= change / 2;
+            removedFromTwos = change / 2;
+            change %= 2;
+        } else{
+            // two are not enought
+            change -= Q * 2;
+            Q = 0; 
+        }
+        if( change <= P){
+            P -= change; 
+            change = 0;
+            maxChilds++;
+        }else{
+            // ones not enought
+            Q += removedFromTwos; 
+        }
+    
+    }
+}
+
 int main(){
     int T;
-    cin>>T;
+    //ifstream fin("lumbybus.in");
 
-    long long int N,P,Q;
+    cin>>T;
+    //fin>>T;
+    
+
     for(int i = 0; i < T; i++){
         cin>>N>>P>>Q;
+        //fin>>N>>P>>Q;
 
-        long long int A[N];
 
+        vector<int> num;
+        int x;
         for(int a = 0; a < N; a++){
-            cin>>A[a];
+            cin>>x;
+            //fin>>x;
+            num.push_back(x);
         }
-        sort(A, A+N);
+        sort(num.begin(), num.end());
 
-        long long int tot = P + (Q*2);
-        int b =0;
-        int maxChilds =0;
-        while(0 < tot && b < N){
-            tot -= A[b];
-            if(tot < 0) break;
-            maxChilds++;
-            if(tot == 0) break;
-
-            b++;
-        }
+        maxChilds =0;
+        solveByGreedy(num);
     
         cout << maxChilds << endl;
     
     }
-
-
 
     return 0;
 }
