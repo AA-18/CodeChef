8ca4c4dca022c84e7bc7f558f8407ba0bd166da9
Aayush Singh Naruka <57344102+AA-18@users.noreply.github.com>
2020-10-01 13:42:01 +0530

    Update Ide.cpp

--- cielab/Ide.cpp
+++ cielab/Ide.cpp
@@ -6,11 +6,13 @@ using namespace std;
 int main() {
     int A,B;
     cin>>A>>B;
-
+    // Checking the last number of difference of A and B.
     int diff = (A-B) % 10;
     if(diff == 9){
+        // If last number of difference is 9 (like 499 , 999) , we will decrease it by one.
         cout << A-B-1 << endl;
     }else{
+       // otherwise increase by one.
         cout << A-B+1 << endl;
     }
 
